65969f85d4c4220fa4c855ea7e7ff4ff06ee9656
Cyrille Perois <cperois@outlook.com>
2018-03-05 15:53:15 +0100

    feat: show maif labs logo in sidebar

---
+++ src/components/BrandLogo/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import maifLabsLogo from 'assets/nestor-labs-bank.svg'
+import styles from './style.styl'
+
+export default () => (
+  <div className={styles.brandlogo}>
+    <img src={maifLabsLogo} alt='' />
+  </div>
+)
